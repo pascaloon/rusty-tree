62ef63be59a5cfcbc424b8bd438ed95042c2b6f6
Pascal <pascalasselin@hotmail.com>
2022-03-11 21:11:24 -0500

    working colors

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::{path::Path, fs::File, io::BufReader, collections::HashMap};
-use serde::Deserialize;
 use serde_derive::Deserialize;
 
 #[derive(Deserialize, Debug)]
@@ -26,6 +25,30 @@ struct IconSet {
     files: FileIconSet
 }
 
+#[derive(Deserialize, Debug)]
+struct DirectoryColorSet {
+    default: String,
+    symlink: String,
+    junction: String,
+    wellknown: HashMap<String, String>
+}
+
+#[derive(Deserialize, Debug)]
+struct FileColorSet {
+    default: String,
+    symlink: String,
+    junction: String,
+    wellknown: HashMap<String, String>,
+    extensions: HashMap<String, String>
+}
+
+
+#[derive(Deserialize, Debug)]
+struct ColorSet {
+    directories: DirectoryColorSet,
+    files: FileColorSet
+}
+
 fn main() {
 
     // let branch_icon = dict.get("nf-dev-git_branch").unwrap();
@@ -37,14 +60,17 @@ fn main() {
 
     let glyphs = load_glyphs();
     let icons = load_icons();
+    let colors = load_colors();
     
     let rust_icon = icons.files.extensions.get(".rs").unwrap();
+    let rust_color = colors.files.extensions.get(".rs").unwrap();
     let rust_glyph = glyphs.get(rust_icon).unwrap();
-    println!("rust icon: {} -> {}", rust_icon, rust_glyph);
+    println!("rust icon: {} -> {} [{}]", rust_icon, rust_glyph, rust_color);
 
     let cs_icon = icons.files.extensions.get(".cs").unwrap();
+    let cs_color = colors.files.extensions.get(".cs").unwrap();
     let cs_glyph = glyphs.get(cs_icon).unwrap();
-    println!("cs icon: {} -> {}", cs_icon, cs_glyph);
+    println!("cs icon: {} -> {} [{}]", cs_icon, cs_glyph, cs_color);
 }
 
 fn load_glyphs() -> HashMap<String, String> {
@@ -56,6 +82,13 @@ fn load_glyphs() -> HashMap<String, String> {
 
 fn load_icons() -> IconSet {
     let path = Path::new("data/icons.json");
+    let file = File::open(path).unwrap();
+    let reader = BufReader::new(file);
+    serde_json::from_reader(reader).unwrap()
+}
+
+fn load_colors() -> ColorSet {
+    let path = Path::new("data/colors.json");
     let file = File::open(path).unwrap();
     let reader = BufReader::new(file);
     serde_json::from_reader(reader).unwrap()
