a251a19a459d4cd315a9866adda6ad4ec0b93bd5
passelin <pascalasselin@hotmail.com>
2024-06-28 22:31:12 -0400

    Separated settings in its own file

--- src/main.rs
+++ src/main.rs
@@ -5,177 +5,65 @@ use flume::{Sender, Receiver};
 use multimap::MultiMap;
 use serde_derive::Deserialize;
 use clap::{Parser};
+use glob_match::glob_match;
+use crate::settings::Config;
 
 mod multimap;
 mod counter;
-
-#[derive(Deserialize, Debug, Clone)]
-struct DirectoryIconSet {
-    default: String,
-    symlink: String,
-    junction: String,
-    wellknown: HashMap<String, String>
-}
-
-#[derive(Deserialize, Debug, Clone)]
-struct FileIconSet {
-    default: String,
-    symlink: String,
-    junction: String,
-    wellknown: HashMap<String, String>,
-    extensions: HashMap<String, String>
-}
+mod settings;
 
 
-#[derive(Deserialize, Debug, Clone)]
-struct IconSet {
-    directories: DirectoryIconSet,
-    files: FileIconSet
-}
-
-#[derive(Deserialize, Debug, Clone)]
-struct DirectoryColorSet {
-    default: String,
-    ignored: String,
-    symlink: String,
-    junction: String,
-    wellknown: HashMap<String, String>
-}
-
-#[derive(Deserialize, Debug, Clone)]
-struct FileColorSet {
-    default: String,
-    symlink: String,
-    junction: String,
-    wellknown: HashMap<String, String>,
-    extensions: HashMap<String, String>
-}
-
-
-#[derive(Deserialize, Debug, Clone)]
-struct ColorSet {
-    directories: DirectoryColorSet,
-    files: FileColorSet
-}
-
-
-#[derive(Deserialize, Debug, Clone)]
-struct Settings {
-    ignored_dirs: Vec<String>,
-    extensions_fold_count: usize
-}
-
-impl Settings {
-    fn is_dir_ignored(&self, path: &PathBuf) -> bool {
-        self.ignored_dirs
-            .iter()
-            .any(|f| path.ends_with(f))
-    }
-}
 
 #[derive(Clone)]
 struct Renderer<'a> {
-    glyphs: HashMap<String, String>,
-    icons: IconSet,
-    colors: ColorSet,
-    settings: &'a Settings
+    pub config: &'a Config
 }
 impl<'a> Renderer<'a> {
     pub fn render_file(&self, path: &PathBuf) {
         let filename_os = path.file_name().unwrap();
         let filename = filename_os.to_str().unwrap();
 
-        let icon = 
-            if let Some(icon) = self.icons.files.wellknown.get(filename) {
-                icon
-            } else if let Some(icon) = Self::find_item_from_extension(&self.icons.files.extensions, filename) {
-                icon
-            } else {
-                &self.icons.files.default
-            };
-
-        let glyph = self.glyphs.get(icon).unwrap();
-
-        let color = 
-            if let Some(color) = self.colors.files.wellknown.get(filename) {
-                color
-            } else if let Some(color) = Self::find_item_from_extension(&self.colors.files.extensions, filename) {
-                color
-            } else {
-                &self.colors.files.default
-        };
-
+        let glyph = self.config.get_associated_file_glyph(filename);
+        let color = self.config.get_associated_file_color(filename);
         let style = hex_to_color(color).normal();
         println!("{} {}", style.paint(glyph), style.paint(filename));
-    }
-
-    fn find_item_from_extension<'b>(map: &'b HashMap<String, String>, filename: &str) -> Option<&'b String> {
-        if let Some(v) = map.get(filename) {
-            return Some(v);
-        }
-        for (pos, _) in filename.match_indices('.') {
-            let pos = pos + 1;
-            if pos < filename.len() {
-                let (_, ext) = filename.split_at(pos);
-                if let Some(v) = map.get(ext) {
-                    return Some(v);
-                }
-            }
-        }
-
-        None
     }
 
     pub fn render_dir(&self, path: &PathBuf, ignored: bool) {
         let filename_os = path.file_name().unwrap();
         let filename = filename_os.to_str().unwrap();
 
-        let icon = 
-            if let Some(icon) = self.icons.directories.wellknown.get(filename) {
-                icon
-            } else {
-                &self.icons.directories.default
-            };
-        let glyph = self.glyphs.get(icon).unwrap();
+        let glyph = self.config.get_associated_dir_glyph(filename);
 
         if ignored {
-            let color = &self.colors.directories.ignored;
+            let color = &self.config.colors.directories.ignored;
             let style = hex_to_color(color).normal();
             println!("{} {}{}", style.paint(glyph), style.paint(filename), style.paint("/..."));
 
         } else {
-            let color = 
-                if let Some(color) = self.colors.directories.wellknown.get(filename) {
-                    color
-                } else {
-                    &self.colors.directories.default
-                };
+            let color = self.config.get_associated_dir_color(filename);
             let style = hex_to_color(color).normal();
             println!("{} {}", style.paint(glyph), style.paint(filename));
         }
     }
 
     pub fn render_skippedfiles(&self, ext: &String, count: i32) {
-        let icon = 
-            if let Some(icon) = self.icons.files.extensions.get(ext) {
-                icon
-            } else {
-                &self.icons.files.default
-            };
+        let glyph = self.config.get_associated_ext_glyph(ext);
 
-        let glyph = self.glyphs.get(icon).unwrap();
-
-        let color = 
-            if let Some(color) = self.colors.files.extensions.get(ext) {
-                color
-            } else {
-                &self.colors.files.default
-        };
-
+        let color = self.config.get_associated_ext_color(ext);
         let style = hex_to_color(color).normal();
+
         let value = format!("{} {} files...", count, ext);
         println!("{} {}", style.paint(glyph), style.paint(value));
     }
+}
+
+
+fn hex_to_color(hex: &String) -> Color {
+    let r = u8::from_str_radix(&hex[0..2], 16).unwrap();
+    let g = u8::from_str_radix(&hex[2..4], 16).unwrap();
+    let b = u8::from_str_radix(&hex[4..6], 16).unwrap();
+    Color::RGB(r, g, b)
 }
 
 enum RenderType {
@@ -200,105 +88,58 @@ struct RenderItem {
     depth: usize
 }
 
-
-
-#[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
-struct Args {
-   /// Name of the person to greet
-   #[clap()]
-   path: Option<String>,
-
-   #[arg(short, long, default_value_t = false)]
-   unfold: bool,
+struct FoundItemEvent {
+    is_dir: bool,
+    is_last: bool,
+    depth: usize,
+    name: PathBuf,
 }
+
+
 
 
 fn main() {
+    let config = Config::load();
 
-    let app_args = Args::parse();
+    let path: PathBuf = config.get_clean_current_path();
+    println!("{}", path.display());
 
-    let path_str = app_args.path.unwrap_or(".".to_string());
-    let unfold = app_args.unfold;
+    let renderer = Renderer {config: &config};
 
-    let path: PathBuf = if Path::new(&path_str).is_absolute() {
-        PathBuf::from(path_str)
-    } else {
-        std::env::current_dir().unwrap().join(path_str)
-    }.canonicalize().unwrap();
-    let path = path.to_str().unwrap().strip_prefix(r"\\?\").unwrap_or(path.to_str().unwrap());
-    println!("{}", path);
-    let path = PathBuf::from_str(path).unwrap();
+    let path_ref = &path;
+    let config_ref = &config;
 
-    let current_exe = std::env::current_exe().unwrap();
-    let data_dir = current_exe.parent().unwrap().join("../..");
-    let data_dir_path = data_dir.as_path();
+    thread::scope(|scope|{
+        let (tx_fs, rx_compute) = flume::unbounded();
+        let (tx_compute, rx_render) = flume::unbounded();
 
-    let glyphs = load_glyphs(data_dir_path);
-    let icons = load_icons(data_dir_path);
-    let colors = load_colors(data_dir_path);
-    let settings = load_settings(data_dir_path);
-    let renderer = Renderer {glyphs, icons, colors, settings: &settings};
+        scope.spawn(move || {
+            list_files2(path_ref, config_ref, 0, &tx_fs);
+        });
+        scope.spawn(move || {
+            compute(config_ref, &rx_compute, &tx_compute);
+        });
 
-    let (tx, rx) = flume::unbounded();
-
-    let path_ref = path;
-    let settings = settings.clone();
-    let tx_ref = tx;
-
-    thread::spawn(move || {
-        list_files(&path_ref, &settings, 0, &tx_ref, unfold);
+        render_files(&renderer, rx_render);
     });
-
-
-    render_files(&renderer, rx);
-}
-
-fn load_glyphs(path: &Path) -> HashMap<String, String> {
-    load_from_file(&path.join("data/glyphs.json"))
-}
-
-fn load_icons(path: &Path) -> IconSet {
-    load_from_file(&path.join("data/icons.json"))
-}
-
-fn load_colors(path: &Path) -> ColorSet {
-    load_from_file(&path.join("data/colors.json"))
-}
-
-fn load_settings(path: &Path) -> Settings {
-    load_from_file(&path.join("data/settings.json"))
-}
-
-fn load_from_file<T>(path: &PathBuf) -> T
-    where T: serde::de::DeserializeOwned,
-{
-    let file = File::open(path).unwrap();
-    let reader = BufReader::new(file);
-    serde_json::from_reader(reader).unwrap()
 }
 
 
-fn hex_to_color(hex: &String) -> Color {
-    let r = u8::from_str_radix(&hex[0..2], 16).unwrap();
-    let g = u8::from_str_radix(&hex[2..4], 16).unwrap();
-    let b = u8::from_str_radix(&hex[4..6], 16).unwrap();
-    Color::RGB(r, g, b)
-}
+
 
 fn render_files(renderer: &Renderer, rx: Receiver<RenderItem>) {
     for item in rx.iter() {
         for _ in 0..item.depth {
-            print!("{}  ", renderer.glyphs.get("pipe-v").unwrap());
+            print!("{}  ", renderer.config.glyphs.get("pipe-v").unwrap());
         }
 
         if item.is_last && item.is_leaf {
-            print!("{}", renderer.glyphs.get("pipe-e").unwrap());
+            print!("{}", renderer.config.glyphs.get("pipe-e").unwrap());
         } else {
-            print!("{}", renderer.glyphs.get("pipe-t").unwrap());
+            print!("{}", renderer.config.glyphs.get("pipe-t").unwrap());
         }
 
-        print!("{} ", renderer.glyphs.get("pipe-h").unwrap());
+        print!("{} ", renderer.config.glyphs.get("pipe-h").unwrap());
         match item.item {
             RenderType::File(f) => renderer.render_file(&f.path),
             RenderType::Dir(d) => renderer.render_dir(&d.path, item.is_leaf),
@@ -307,7 +148,23 @@ fn render_files(renderer: &Renderer, rx: Receiver<RenderItem>) {
     }
 }
 
-fn list_files(path: &PathBuf, settings: &Settings, depth: usize, tx: &Sender<RenderItem>, unfold: bool) {
+fn compute(config: &Config, rx_compute: &Receiver<FoundItemEvent>, tx_compute: &Sender<RenderItem>) {
+    for item in rx_compute.iter() {
+        let i = match item.is_dir {
+            true => RenderType::Dir(FileRenderItem { path: item.name }),
+            false => RenderType::File(FileRenderItem { path: item.name}),
+        };
+        tx_compute.send(RenderItem {
+            item: i,
+            depth: item.depth,
+            is_leaf: false,
+            is_last: item.is_last
+        }).unwrap();
+    }
+}
+
+
+fn list_files2(path: &PathBuf, config: &Config, depth: usize, tx_fs: &Sender<FoundItemEvent>) {
     let paths = fs::read_dir(path).unwrap();
     let mut files: Vec<DirEntry> = Vec::with_capacity(32);
     let mut dirs: Vec<DirEntry> = Vec::with_capacity(32);
@@ -321,88 +178,148 @@ fn list_files(path: &PathBuf, settings: &Settings, depth: usize, tx: &Sender<Ren
     }
 
     let total = files.len() + dirs.len();
-    let mut c = 0;    
+    let mut c = 0;
 
-    if unfold || files.len() < settings.extensions_fold_count {
-        for file in files {
-            c +=1;
-            let is_last = c == total;
-    
-            tx.send(RenderItem {
-                item: RenderType::File(FileRenderItem { path: file.path() }),
-                depth: depth,
-                is_leaf: true,
-                is_last: is_last
-            }).unwrap();
-        }
-    } else {
-        
-        // Find all extensions
-        let mut paths: Vec<(PathBuf, Option<String>)> = Vec::with_capacity(files.len());
-        let mut counter: Counter<String> = Counter::new();
-        for file in files {
-            let path = file.path();
-            if let Some(ext) = path.extension() {
-                let ext = ext.to_str().unwrap().to_string();
-                counter.inc(&ext);
-                paths.push((path, Some(ext)));
-            } else {
-                paths.push((path, None));
-            }
-        }
-        
-        let max_fold = settings.extensions_fold_count as i32;
+    for file in files {
+        c +=1;
+        let is_last = c == total;
 
-        for (k, v) in counter.iter() {
-            if *v >= max_fold {
-                c += std::cmp::max(0, *v) as usize;
-                let is_last = c == total;
-
-                tx.send(RenderItem {
-                    item: RenderType::SkppedFiles(SkippedRenderIten { ext: k.clone(), count: *v }),
-                    depth: depth,
-                    is_leaf: true,
-                    is_last: is_last
-                }).unwrap();
-            }
-        }
-
-        for (path, ext) in paths {
-            let display = match ext {
-                Some(ext) => counter.get(&ext).map_or(true, |c| c < max_fold),
-                None => true,
-            };
-
-            if display {
-                c += 1;
-                let is_last = c == total;
-
-                tx.send(RenderItem {
-                    item: RenderType::File(FileRenderItem { path }),
-                    depth: depth,
-                    is_leaf: true,
-                    is_last: is_last
-                }).unwrap();
-            }
-        }
+        tx_fs.send(FoundItemEvent {
+            is_dir: false,
+            depth: depth,
+            name: file.path(),
+            is_last: is_last
+        }).unwrap();
     }
 
     for dir in dirs {
         let path = dir.path();
-        let is_ignored = settings.is_dir_ignored(&path);
+        let is_ignored = config.is_dir_ignored(&path);
 
         c +=1;
         let is_last = c == total;
 
-        tx.send(RenderItem {
-            item: RenderType::Dir(FileRenderItem { path }),
+        tx_fs.send(FoundItemEvent {
+            is_dir: true,
             depth: depth,
-            is_leaf: is_ignored,
+            name: dir.path(),
             is_last: is_last
         }).unwrap();
 
         if !is_ignored {
-            list_files(&dir.path(), settings, depth+1, tx, unfold);
+            crate::list_files2(&dir.path(), config, depth+1, tx_fs);
         }
     }
 }
+
+
+//
+// fn list_files(path: &PathBuf, settings: &Settings, depth: usize, tx: &Sender<RenderItem>, unfold: bool, globbing: &Option<String>) {
+//     let paths = fs::read_dir(path).unwrap();
+//     let mut files: Vec<DirEntry> = Vec::with_capacity(32);
+//     let mut dirs: Vec<DirEntry> = Vec::with_capacity(32);
+//     for path in paths {
+//         let path = path.unwrap();
+//         if path.file_type().unwrap().is_dir() {
+//             dirs.push(path);
+//         } else {
+//             if let Some(globbing) = &globbing {
+//                 let path_buff = path.file_name();
+//                 let path_str = path_buff.to_str().unwrap();
+//                 // println!("{path_str}");
+//                 if glob_match(globbing, path_str) {
+//                     files.push(path);
+//                 }
+//             }
+//             else {
+//                 files.push(path);
+//             }
+//         }
+//     }
+//
+//     let total = files.len() + dirs.len();
+//     let mut c = 0;
+//
+//     if unfold || files.len() < settings.extensions_fold_count {
+//         for file in files {
+//             c +=1;
+//             let is_last = c == total;
+//
+//             tx.send(RenderItem {
+//                 item: RenderType::File(FileRenderItem { path: file.path() }),
+//                 depth: depth,
+//                 is_leaf: true,
+//                 is_last: is_last
+//             }).unwrap();
+//         }
+//     } else {
+//
+//         // Find all extensions
+//         let mut paths: Vec<(PathBuf, Option<String>)> = Vec::with_capacity(files.len());
+//         let mut counter: Counter<String> = Counter::new();
+//         for file in files {
+//             let path = file.path();
+//             if let Some(ext) = path.extension() {
+//                 let ext = ext.to_str().unwrap().to_string();
+//                 counter.inc(&ext);
+//                 paths.push((path, Some(ext)));
+//             } else {
+//                 paths.push((path, None));
+//             }
+//         }
+//
+//         let max_fold = settings.extensions_fold_count as i32;
+//
+//         for (k, v) in counter.iter() {
+//             if *v >= max_fold {
+//                 c += std::cmp::max(0, *v) as usize;
+//                 let is_last = c == total;
+//
+//                 tx.send(RenderItem {
+//                     item: RenderType::SkppedFiles(SkippedRenderIten { ext: k.clone(), count: *v }),
+//                     depth: depth,
+//                     is_leaf: true,
+//                     is_last: is_last
+//                 }).unwrap();
+//             }
+//         }
+//
+//         for (path, ext) in paths {
+//             let display = match ext {
+//                 Some(ext) => counter.get(&ext).map_or(true, |c| c < max_fold),
+//                 None => true,
+//             };
+//
+//             if display {
+//                 c += 1;
+//                 let is_last = c == total;
+//
+//                 tx.send(RenderItem {
+//                     item: RenderType::File(FileRenderItem { path }),
+//                     depth: depth,
+//                     is_leaf: true,
+//                     is_last: is_last
+//                 }).unwrap();
+//             }
+//         }
+//     }
+//
+//     for dir in dirs {
+//         let path = dir.path();
+//         let is_ignored = settings.is_dir_ignored(&path);
+//
+//         c +=1;
+//         let is_last = c == total;
+//
+//         tx.send(RenderItem {
+//             item: RenderType::Dir(FileRenderItem { path }),
+//             depth: depth,
+//             is_leaf: is_ignored,
+//             is_last: is_last
+//         }).unwrap();
+//
+//         if !is_ignored {
+//             list_files(&dir.path(), settings, depth+1, tx, unfold, globbing);
+//         }
+//     }
+// }
---
+++ src/settings.rs
@@ -0,0 +1,229 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::{Path, PathBuf};
+use std::str::FromStr;
+use clap::Parser;
+use serde_derive::Deserialize;
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct DirectoryIconSet {
+    pub default: String,
+    pub symlink: String,
+    pub junction: String,
+    pub wellknown: HashMap<String, String>
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct FileIconSet {
+    pub default: String,
+    pub symlink: String,
+    pub junction: String,
+    pub wellknown: HashMap<String, String>,
+    pub extensions: HashMap<String, String>
+}
+
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct IconSet {
+    pub directories: DirectoryIconSet,
+    pub files: FileIconSet
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct DirectoryColorSet {
+    pub default: String,
+    pub ignored: String,
+    pub symlink: String,
+    pub junction: String,
+    pub wellknown: HashMap<String, String>
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct FileColorSet {
+    pub default: String,
+    pub symlink: String,
+    pub junction: String,
+    pub wellknown: HashMap<String, String>,
+    pub extensions: HashMap<String, String>
+}
+
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct ColorSet {
+    pub directories: DirectoryColorSet,
+    pub files: FileColorSet
+}
+
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+pub struct Args {
+    /// Name of the person to greet
+    #[clap()]
+    pub path: Option<String>,
+
+    #[arg(short, long, default_value_t = false)]
+    pub unfold: bool,
+
+    #[arg(short, long)]
+    pub filter: Option<String>,
+}
+
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct Settings {
+    pub ignored_dirs: Vec<String>,
+    pub extensions_fold_count: usize
+}
+
+pub struct Config {
+    pub settings: Settings,
+    pub glyphs: HashMap<String, String>,
+    pub icons: IconSet,
+    pub colors: ColorSet,
+    pub args: Args,
+
+}
+
+fn load_glyphs(path: &Path) -> HashMap<String, String> {
+    load_from_file(&path.join("data/glyphs.json"))
+}
+
+fn load_icons(path: &Path) -> IconSet {
+    load_from_file(&path.join("data/icons.json"))
+}
+
+fn load_colors(path: &Path) -> ColorSet {
+    load_from_file(&path.join("data/colors.json"))
+}
+
+fn load_settings(path: &Path) -> Settings {
+    load_from_file(&path.join("data/settings.json"))
+}
+
+fn load_from_file<T>(path: &PathBuf) -> T
+    where T: serde::de::DeserializeOwned,
+{
+    let file = File::open(path).unwrap();
+    let reader = BufReader::new(file);
+    serde_json::from_reader(reader).unwrap()
+}
+
+fn get_data_dir_path() -> PathBuf {
+    let current_exe = std::env::current_exe().unwrap();
+    let data_dir = current_exe.parent().unwrap().join("../..");
+    data_dir
+}
+
+
+/// A utility method that tries to find the **longest extension matching the filename**
+fn find_item_from_extension<'b>(map: &'b HashMap<String, String>, filename: &str) -> Option<&'b String> {
+    if let Some(v) = map.get(filename) {
+        return Some(v);
+    }
+    for (pos, _) in filename.match_indices('.') {
+        let pos = pos + 1;
+        if pos < filename.len() {
+            let (_, ext) = filename.split_at(pos);
+            if let Some(v) = map.get(ext) {
+                return Some(v);
+            }
+        }
+    }
+
+    None
+}
+
+impl Config {
+    pub fn load() -> Self {
+        let data_dir = get_data_dir_path();
+        let data_dir = data_dir.as_path();
+        Config {
+            settings: load_settings(data_dir),
+            glyphs: load_glyphs(data_dir),
+            icons: load_icons(data_dir),
+            colors: load_colors(data_dir),
+            args: Args::parse(),
+        }
+    }
+
+    pub fn is_dir_ignored(&self, path: &PathBuf) -> bool {
+        self.settings.ignored_dirs
+            .iter()
+            .any(|f| path.ends_with(f))
+    }
+
+    pub fn get_clean_current_path(&self) -> PathBuf {
+        let path_str = self.args.path.clone().unwrap_or(".".to_string());
+        let path = if Path::new(&path_str).is_absolute() {
+            PathBuf::from(path_str)
+        } else {
+            std::env::current_dir().unwrap().join(path_str)
+        }.canonicalize().unwrap();
+        let path = path.to_str().unwrap().strip_prefix(r"\\?\").unwrap_or(path.to_str().unwrap());
+        PathBuf::from_str(path).unwrap()
+    }
+
+    pub fn get_associated_ext_glyph(&self, ext: &str) -> &String {
+        let icon =
+            if let Some(icon) = self.icons.files.extensions.get(ext) {
+                icon
+            } else {
+                &self.icons.files.default
+            };
+        self.glyphs.get(icon).unwrap()
+    }
+
+    pub fn get_associated_ext_color(&self, ext: &str) -> &String {
+        if let Some(color) = self.colors.files.extensions.get(ext) {
+            color
+        } else {
+            &self.colors.files.default
+        }
+    }
+
+    pub fn get_associated_file_glyph(&self, filename: &str) -> &String {
+        let icon =
+            if let Some(icon) = self.icons.files.wellknown.get(filename) {
+                icon
+            } else if let Some(icon) = find_item_from_extension(&self.icons.files.extensions, filename) {
+                icon
+            } else {
+                &self.icons.files.default
+            };
+        self.glyphs.get(icon).unwrap()
+    }
+
+    pub fn get_associated_file_color(&self, filename: &str) -> &String {
+        if let Some(color) = self.colors.files.wellknown.get(filename) {
+            color
+        } else if let Some(color) = find_item_from_extension(&self.colors.files.extensions, filename) {
+            color
+        } else {
+            &self.colors.files.default
+        }
+    }
+
+    pub fn get_associated_dir_glyph(&self, filename: &str) -> &String {
+        let icon =
+            if let Some(icon) = self.icons.directories.wellknown.get(filename) {
+                icon
+            } else {
+                &self.icons.files.default
+            };
+        self.glyphs.get(icon).unwrap()
+    }
+
+    pub fn get_associated_dir_color(&self, filename: &str) -> &String {
+        if let Some(color) = self.colors.directories.wellknown.get(filename) {
+            color
+        } else {
+            &self.colors.files.default
+        }
+    }
+
+
+
+
+}
